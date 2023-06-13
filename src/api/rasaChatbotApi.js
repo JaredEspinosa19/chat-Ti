baf76ce30c6491d0cf76bbffbe438b5c3e5b7748
Jared Espinosa <ejec-2001@hotmail.com>
2023-06-12 18:50:12 -0600

    Cambios variables

--- src/api/rasaChatbotApi.js
+++ src/api/rasaChatbotApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { getEnvVariables } from "../helpers";
 
-// const {VITE_API_URL} = getEnvVariables();
+const {VITE_API_URL} = getEnvVariables();
 
 const rasaChatbotApi = axios.create({
-  baseURL: 'https://0633-189-156-160-123.ngrok-free.app/api',
+  baseURL: VITE_API_URL,
 })
 
 
